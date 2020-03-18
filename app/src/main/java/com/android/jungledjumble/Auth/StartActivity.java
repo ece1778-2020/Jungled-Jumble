63cfb8e9416f3fb11787a65e6d70ef092ea5e1cb
saidbanos <60584839+saidbanos@users.noreply.github.com>
2020-03-17 20:11:09 -0400

    Splash screen

--- app/src/main/java/com/android/jungledjumble/Auth/StartActivity.java
+++ app/src/main/java/com/android/jungledjumble/Auth/StartActivity.java
@@ -32,6 +32,7 @@ public class StartActivity extends AppCompatActivity {
     }
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
