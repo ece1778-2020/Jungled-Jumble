b97e754c01f1656cbc48285caa19ccfb0346cd7f
saidbanos <60584839+saidbanos@users.noreply.github.com>
2020-03-28 00:31:35 -0400

    Tutorial screen update

--- app/src/main/java/com/android/jungledjumble/Setting/Tutorial.java
+++ app/src/main/java/com/android/jungledjumble/Setting/Tutorial.java
@@ -34,7 +34,8 @@ public class Tutorial extends AppCompatActivity {
 
 FrameLayout frameLay1,frameLay2,frameLay3,frameLay4,frameLay5,frameLay6,frameLay7,frameLay8,frameLay9;
 ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_pointing5, hand_tapping5, left8, right8, hand_pointing8, hand_tapping8, points_game3, points_game6, lives_1_3,lives_2_3,lives_3_3,lives_1_6,lives_2_6,lives_3_6;
-
+ImageView next_level_pic_right1,next_level_pic_right3,next_level_pic_right9,next_level_pic_left1,next_level_pic_left3,next_level_pic_left9;
+TextView fruits_collected3,fruits_collected6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
@@ -77,20 +78,53 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
         lives_1_6 = findViewById (R.id.lives_1_6);
         lives_1_6 = findViewById (R.id.lives_1_6);
 
+        fruits_collected3 = findViewById (R.id.fruits_collected3);
+        fruits_collected6 = findViewById (R.id.fruits_collected6);
+
+        next_level_pic_right1 = findViewById (R.id.next_level_pic_right1);
+        next_level_pic_right3 = findViewById (R.id.next_level_pic_right3);
+        next_level_pic_right9 = findViewById (R.id.next_level_pic_right9);
+        next_level_pic_left1 = findViewById (R.id.next_level_pic_left1);
+        next_level_pic_left3 = findViewById (R.id.next_level_pic_left3);
+        next_level_pic_left9 = findViewById (R.id.next_level_pic_left9);
+
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 // Do something after delay
 
+                next_level_pic_right1.setVisibility(View.VISIBLE);
+                Animation RotateRight = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_right);
+                next_level_pic_right1.startAnimation(RotateRight);
+                next_level_pic_left1.setVisibility(View.VISIBLE);
+                Animation RotateLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_left);
+                next_level_pic_left1.startAnimation(RotateLeft);
+
+
                 frameLay1.setVisibility(View.GONE);
                 frameLay2.setVisibility(View.VISIBLE);
 
+
+
                 hand_pointing2.setVisibility(View.VISIBLE);
-                hand_tapping2.setVisibility(View.GONE);
                 Animation TranslateDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_down);
                 hand_pointing2.startAnimation(TranslateDown);
                 TranslateDown.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation Animation) {}
+                    @Override
+                    public void onAnimationRepeat(Animation Animation) {}
+                    @Override
+                    public void onAnimationEnd(Animation Animation) {
+                    }
+                });
+
+
+                hand_tapping2.setVisibility(View.VISIBLE);
+                Animation FadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+                hand_tapping2.startAnimation(FadeIn);
+                FadeIn.setAnimationListener(new Animation.AnimationListener() {
                     @Override
                     public void onAnimationStart(Animation Animation) {}
                     @Override
@@ -117,15 +151,27 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
         right2.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
 
+                next_level_pic_right3.setVisibility(View.VISIBLE);
+                Animation RotateRight = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_right);
+                next_level_pic_right3.startAnimation(RotateRight);
+                next_level_pic_left3.setVisibility(View.VISIBLE);
+                Animation RotateLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_left);
+                next_level_pic_left3.startAnimation(RotateLeft);
+
                 frameLay2.setVisibility(View.GONE);
                 frameLay3.setVisibility(View.VISIBLE);
 
-                points_game3.setVisibility(View.VISIBLE);
                 Animation TranslateInto = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_into);
+
+                points_game3.setVisibility(View.VISIBLE);
                 points_game3.startAnimation(TranslateInto);
 
                 lives_2_3.setVisibility(View.VISIBLE);
                 lives_2_3.startAnimation(TranslateInto);
+
+                fruits_collected3.setVisibility(View.VISIBLE);
+                fruits_collected3.startAnimation(TranslateInto);
+
 
 
                 final Handler handler = new Handler();
@@ -151,6 +197,19 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
                                 Animation TranslateDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_down);
                                 hand_pointing5.startAnimation(TranslateDown);
                                 TranslateDown.setAnimationListener(new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation Animation) {}
+                                    @Override
+                                    public void onAnimationRepeat(Animation Animation) {}
+                                    @Override
+                                    public void onAnimationEnd(Animation Animation) {
+                                    }
+                                });
+
+                                hand_tapping5.setVisibility(View.VISIBLE);
+                                Animation FadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+                                hand_tapping5.startAnimation(FadeIn);
+                                FadeIn.setAnimationListener(new Animation.AnimationListener() {
                                     @Override
                                     public void onAnimationStart(Animation Animation) {}
                                     @Override
@@ -186,12 +245,16 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
                 frameLay5.setVisibility(View.GONE);
                 frameLay6.setVisibility(View.VISIBLE);
 
-                points_game6.setVisibility(View.VISIBLE);
                 Animation TranslateInto = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_into);
+
+                points_game6.setVisibility(View.VISIBLE);
                 points_game6.startAnimation(TranslateInto);
 
                 lives_1_6.setVisibility(View.VISIBLE);
                 lives_1_6.startAnimation(TranslateInto);
+
+                fruits_collected6.setVisibility(View.VISIBLE);
+                fruits_collected6.startAnimation(TranslateInto);
 
                 final Handler handler = new Handler();
                 handler.postDelayed(new Runnable() {
@@ -216,6 +279,19 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
                                 Animation TranslateDown = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.translate_down);
                                 hand_pointing8.startAnimation(TranslateDown);
                                 TranslateDown.setAnimationListener(new Animation.AnimationListener() {
+                                    @Override
+                                    public void onAnimationStart(Animation Animation) {}
+                                    @Override
+                                    public void onAnimationRepeat(Animation Animation) {}
+                                    @Override
+                                    public void onAnimationEnd(Animation Animation) {
+                                    }
+                                });
+
+                                hand_tapping8.setVisibility(View.VISIBLE);
+                                Animation FadeIn = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade_in);
+                                hand_tapping8.startAnimation(FadeIn);
+                                FadeIn.setAnimationListener(new Animation.AnimationListener() {
                                     @Override
                                     public void onAnimationStart(Animation Animation) {}
                                     @Override
@@ -254,6 +330,13 @@ ImageView left2, right2, hand_pointing2, hand_tapping2, left5, right5, hand_poin
                     @Override
                     public void run() {
                         // Do something after delay
+
+                        next_level_pic_right9.setVisibility(View.VISIBLE);
+                        Animation RotateRight = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_right);
+                        next_level_pic_right9.startAnimation(RotateRight);
+                        next_level_pic_left9.setVisibility(View.VISIBLE);
+                        Animation RotateLeft = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotate_left);
+                        next_level_pic_left9.startAnimation(RotateLeft);
 
                         startActivity(new Intent (Tutorial.this, StartActivity.class));
 
